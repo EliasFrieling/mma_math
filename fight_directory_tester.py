0a15d34d45ed2d3c1ff04c3e89b580c2a433d7e7
EliasFrieling <eliasfrieling@gmail.com>
2018-12-28 16:51:04 -0500

    Update fight_directory_tester.py

--- fight_directory_tester.py
+++ fight_directory_tester.py
@@ -12,11 +12,11 @@ class Test(unittest.TestCase):
 
     def test01(self):
         myDict = {'Randy Coture' : [['Pedro Rizzo', ['win', 'win']], ['Josh Barnett', ['loss']], ['sample', ['win']]]}
-        myBool = fight_directory.rematch('Randy Coture', 'Josh Barnett', myDict)
+        myBool = fight_directory.is_rematch('Randy Coture', 'Josh Barnett', myDict)
         self.assertEqual(myBool, True)
     def test02(self):
         myDict = {'Randy Coture' : [['Pedro Rizzo', ['win', 'win']], ['Josh Barnett', ['loss']], ['sample', ['win']]]}
-        myBool = fight_directory.rematch('Randy Coture', 'Pedro Rizzo', myDict)
+        myBool = fight_directory.is_rematch('Randy Coture', 'Pedro Rizzo', myDict)
         self.assertEqual(myBool, True)
 
 if __name__ == "__main__":
